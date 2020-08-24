e86627f6bfc44833b9cddcb87afe478efd845fc0
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2020-08-24 16:51:26 +0800

    develop credentials

--- credential/php/src/PDSCredentials.php
+++ credential/php/src/PDSCredentials.php
@@ -1,13 +1,16 @@
 <?php
 
-// This file is auto-generated, don't edit it. Thanks.
-
 namespace Aliyun\PDS\Credentials;
 
-use Exception;
+use Adbar\Dot;
+use AlibabaCloud\Tea\Exception\TeaError;
+use AlibabaCloud\Tea\Request;
+use AlibabaCloud\Tea\Tea;
 
 class PDSCredentials
 {
+    public $httpConfig = [];
+
     protected $_endpoint;
 
     protected $_domainId;
@@ -78,6 +81,64 @@ class PDSCredentials
      */
     public function getAccessToken()
     {
-        throw new Exception('Un-implemented');
+        if (null === $this->_accessToken) {
+            throw new \RuntimeException('AccessToken is null');
+        }
+        if ($this->shouldRefresh(time())) {
+            $this->refreshAccessToken();
+        }
+
+        return $this->_accessToken;
+    }
+
+    public function shouldRefresh($now)
+    {
+        if (empty($this->_expireTime) || empty($this->_refreshToken)) {
+            return false;
+        }
+
+        return strtotime($this->_expireTime) - $now <= 500;
+    }
+
+    private function refreshAccessToken()
+    {
+        $request           = new Request();
+        $request->protocol = 'http';
+        $request->method   = 'POST';
+        $request->pathname = '/v2/oauth/token';
+        $request->headers  = [
+            'host'                    => $this->getHost($this->_endpoint, $this->_domainId . '.api.aliyunpds.com'),
+            'content-type'            => 'application/x-www-form-urlencoded; charset=utf-8',
+            'date'                    => gmdate('Y-m-d\\TH:i:s\\Z'),
+            'accept'                  => 'application/json',
+            'x-acs-signature-method'  => 'HMAC-SHA1',
+            'x-acs-signature-version' => '1.0',
+        ];
+        $bodyString        = 'grant_type=refresh_token&refresh_token=' . $this->_refreshToken
+            . '&client_id=' . $this->_clientId
+            . '&client_secret=' . $this->_clientSecret;
+        $request->body     = $bodyString;
+        $response          = Tea::send($request, $this->httpConfig);
+        if ($response->getBody()->isSeekable()) {
+            $response->getBody()->rewind();
+        }
+        $contents = $response->getBody()->getContents();
+        if (200 === $response->getStatusCode()) {
+            $result              = new Dot(json_decode($contents, true));
+            $this->_expireTime   = $result->get('expire_time');
+            $this->_accessToken  = $result->get('access_token');
+            $this->_refreshToken = $result->get('refresh_token');
+        } else {
+            throw new TeaError(json_decode($contents));
+        }
+    }
+
+    private function getHost($endpoint, $domain)
+    {
+        if (null !== $endpoint && \strlen(trim($endpoint)) > 0) {
+            return $endpoint;
+        }
+
+        return $domain;
     }
 }
