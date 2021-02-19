70814dc117cbb6eb0926f01d6379a667c7afcb0a
jhgaog <731746659@qq.com>
2021-02-19 10:24:28 +0800

    regenerate pds csharp

--- pds/csharp/core/Models/ListFacegroupsModel.cs
+++ pds/csharp/core/Models/ListFacegroupsModel.cs
@@ -8,7 +8,7 @@ using Tea;
 
 namespace Aliyun.SDK.PDS.Client.Models
 {
-    public class ListFaceGroupsModel : TeaModel {
+    public class ListFacegroupsModel : TeaModel {
         [NameInMap("headers")]
         [Validation(Required=false)]
         public Dictionary<string, string> Headers { get; set; }
